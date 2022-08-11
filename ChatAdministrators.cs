5ef5a5be7343b0cfcdfdb7cea153e6c592f7ded2
VladS23 <77384665+VladS23@users.noreply.github.com>
2022-08-11 22:54:01 +0300

    auto adm status

---
+++ ChatAdministrators.cs
@@ -0,0 +1,35 @@
+﻿namespace TgAdmBot
+{
+    internal class ChatAdministrators
+    {
+        public bool ok { get; set; }
+        public Result[] result { get; set; }
+    }
+
+    public class Result
+    {
+        public User user { get; set; }
+        public string status { get; set; }
+        public bool can_be_edited { get; set; }
+        public bool can_manage_chat { get; set; }
+        public bool can_change_info { get; set; }
+        public bool can_delete_messages { get; set; }
+        public bool can_invite_users { get; set; }
+        public bool can_restrict_members { get; set; }
+        public bool can_pin_messages { get; set; }
+        public bool can_promote_members { get; set; }
+        public bool can_manage_video_chats { get; set; }
+        public bool is_anonymous { get; set; }
+        public bool can_manage_voice_chats { get; set; }
+    }
+
+    public class User
+    {
+        public long id { get; set; }
+        public bool is_bot { get; set; }
+        public string first_name { get; set; }
+        public string username { get; set; }
+        public string language_code { get; set; }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@ using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using MySql.Data.MySqlClient;
+using System.Net.Http;
 
 namespace TgAdmBot
 {
     class Program
     {
-        private static ITelegramBotClient bot = new TelegramBotClient("5328960378:AAH1fskxZcH3GEXMVKHROuhfnNJQsSj8gvU");
+        private static string botToken = "5328960378:AAH1fskxZcH3GEXMVKHROuhfnNJQsSj8gvU";
+        private static ITelegramBotClient bot = new TelegramBotClient(botToken);
         private static MySqlConnection conn = new MySqlConnection("server=localhost; port=3306; username=root; password=root; database=tgadmbot");
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
@@ -20,37 +22,43 @@ namespace TgAdmBot
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 var message = update.Message;
-                Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(message.Chat));
+                //Console.WriteLine(message.From.);
+                //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(message.Chat));
                 if (!isThisChatInDB(message))
                 {
                     CreateThisChatInDb(message);
                 }
+               // Console.WriteLine(isThisUserInDB(message));
                 if (!isThisUserInDB(message))
                 {
+                    Console.WriteLine("not in db");
                     CreateThisUserInDB(message);
                 }
                 if (message.Text != null)
                 {
-                    Console.WriteLine(message.From.Id);
+                    //Console.WriteLine(message.From.Id);
                     if (message.Text.ToLower()[0] == '/')
                     {
-                        if (message.Text.ToLower().Trim() == "/voicemessange")
+                        if (AdminStatus(message.Chat.Id, message.From.Id)== "creator" || AdminStatus(message.Chat.Id, message.From.Id)== "administrator")
                         {
-                            voiceMessangeCommand(message);
-                            if (isVoiceMessengeBlocked(message) == true)
+                            if (message.Text.ToLower().Trim() == "/voicemessange")
                             {
-                                await botClient.SendTextMessageAsync(message.Chat, "Теперь в данной беседе запрещены голосовые сообщения");
+                                voiceMessangeCommand(message);
+                                if (isVoiceMessengeBlocked(message) == true)
+                                {
+                                    await botClient.SendTextMessageAsync(message.Chat, "Теперь в данной беседе запрещены голосовые сообщения");
+                                }
+                                else
+                                {
+                                    await botClient.SendTextMessageAsync(message.Chat, "Теперь в данной беседе разрешены голосовые сообщения");
+                                }
+                                return;
                             }
                             else
                             {
-                                await botClient.SendTextMessageAsync(message.Chat, "Теперь в данной беседе разрешены голосовые сообщения");
+                                await botClient.SendTextMessageAsync(message.Chat, "Неизвестная комманда");
+                                return;
                             }
-                            return;
-                        }
-                        else
-                        {
-                            await botClient.SendTextMessageAsync(message.Chat, "Неизвестная комманда");
-                            return;
                         }
                     }
                 }
@@ -62,6 +70,64 @@ namespace TgAdmBot
             }
         }
 
+        private static string AdminStatus(long chatid, long userid)
+        {
+            string sql = "SELECT Admin FROM users WHERE ID =" + (chatid.ToString() + userid.ToString());
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            string adminStatus = (string)cmd.ExecuteScalar();
+            //Console.WriteLine(adminStatus);
+            if (adminStatus!="0")
+            {
+                return adminStatus;
+            }
+            else
+            {
+                using (HttpClientHandler hld = new HttpClientHandler())
+                {
+                    using (HttpClient cln = new HttpClient())
+                    {
+                        using (var resp = cln.GetAsync($"https://api.telegram.org/bot" + botToken + $"/getChatAdministrators?chat_id=" + chatid.ToString()).Result)
+                        {
+                            var json=resp.Content.ReadAsStringAsync().Result;
+                            if (!string.IsNullOrEmpty(json))
+                            {
+                                ChatAdministrators admins = Newtonsoft.Json.JsonConvert.DeserializeObject<ChatAdministrators>(json);
+                                if (admins.result != null)
+                                {
+                                    foreach (var admin in admins.result)
+                                    {
+                                        if (admin.user.id == userid)
+                                        {
+                                            sql = $"UPDATE `users` SET `Admin` = '{admin.status}' WHERE `users`.`ID` = {chatid.ToString() + userid.ToString()};";
+                                            cmd = new MySqlCommand(sql, conn);
+                                            int rowCount = cmd.ExecuteNonQuery();
+                                            return admin.status;
+
+                                        }
+                                        else
+                                        {
+                                            sql = $"UPDATE `users` SET `Admin` = 'normal' WHERE `users`.`ID` = {chatid.ToString() + userid.ToString()};";
+                                            cmd = new MySqlCommand(sql, conn);
+                                            int rowCount = cmd.ExecuteNonQuery();
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    return "normal";
+                                }
+                            }
+                            else
+                            {
+                                return "normal";
+                            }
+                        }
+                    }
+                }
+                return "normal";
+            }
+        }
+
         private static void CreateThisUserInDB(Message message)
         {
             string sql = $"INSERT INTO `users` (`Number`, `ID`, `Admin`) VALUES (NULL, {message.Chat.Id.ToString()+message.From.Id.ToString()}, '0');";
@@ -70,7 +136,11 @@ namespace TgAdmBot
         }
         private static bool isThisUserInDB(Message message)
         {
-            string sql = $"SELECT COUNT({message.Chat.Id.ToString() + message.From.Id.ToString()}) FROM `users` WHERE 1";
+            Console.WriteLine("working");
+            //string sql = "SELECT Count(id) FROM chats WHERE id = " + message.Chat.Id;
+
+            string sql = $"SELECT COUNT(id) FROM `users` WHERE id="+message.Chat.Id.ToString() + message.From.Id.ToString();
+            Console.WriteLine(sql);
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             bool isThisUserInDB = false;
             long req = (Int64)cmd.ExecuteScalar();
@@ -84,7 +154,7 @@ namespace TgAdmBot
         private static bool isVoiceMessengeBlocked(Telegram.Bot.Types.Message message)
         {
             string sql = "SELECT voiceMessangeBlock FROM chats WHERE ID =" + message.Chat.Id;
-            Console.WriteLine(message.Chat.Id);
+            //Console.WriteLine(message.Chat.Id);
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             bool isVoiseBlock =(bool)cmd.ExecuteScalar();
             return isVoiseBlock;
@@ -96,7 +166,7 @@ namespace TgAdmBot
             string sql = $"INSERT INTO `chats` (`Number`, `ID`, `IsVIP`, `voiceMessangeBlock`) VALUES (NULL, {message.Chat.Id}, '0', '0');";
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             int rowCount = cmd.ExecuteNonQuery();
-            Console.WriteLine("Row Count affected = " + rowCount);
+            //Console.WriteLine("Row Count affected = " + rowCount);
         }
 
         private static bool isThisChatInDB(Telegram.Bot.Types.Message message)
@@ -116,7 +186,7 @@ namespace TgAdmBot
         private async static void voiceMessangeCommand(Telegram.Bot.Types.Message message)
         {
             string sql = "SELECT voiceMessangeBlock FROM chats WHERE ID =" + message.Chat.Id;
-            Console.WriteLine(message.Chat.Id);
+            //Console.WriteLine(message.Chat.Id);
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             bool isVoiseBlock = (bool)cmd.ExecuteScalar();
             if (isVoiseBlock)
