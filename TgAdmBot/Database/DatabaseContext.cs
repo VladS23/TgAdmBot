11d41dc015c270be8956fcb12922977d85a6d484
AlexC-ux <eunv2004@gmail.com>
2022-12-21 01:29:07 +0300

    Пиздец код
    Я там примеров как пользоваться базой накидал в твой год, но всё надо поправить.... Проще с нуля переписать...
    
    Жду новый Program.cs на выхах)

---
+++ TgAdmBot/Database/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TgAdmBot.Database
+{
+    public class BotDatabase
+    {
+        public static Database? db = null;
+        public BotDatabase()
+        {
+            if (db == null)
+            {
+                db = new Database();
+                db.Database.EnsureCreated();
+            }
+        }
+        public class Database : DbContext
+        {
+            public DbSet<User> Users { get; set; }
+            public DbSet<Chat> Chats { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlite($"Data Source=database.db");
+            }
+        }
+    }
+}
