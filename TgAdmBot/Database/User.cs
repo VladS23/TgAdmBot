e87ddf1106c04dd61bb330cdc5ed86bdbc7e440a
VladS23 <77384665+VladS23@users.noreply.github.com>
2022-12-23 23:57:34 +0300

    mutfix

--- TgAdmBot/Database/User.cs
+++ TgAdmBot/Database/User.cs
@@ -132,25 +132,25 @@ namespace TgAdmBot.Database
                     Ban();
                     return "Пользователь был удален в связи с превышением лимита предупреждений";
                 case WarnsLimitAction.mute:
-                    Mute();
+                    Mute(24*60);
                     return "Пользователю запрещено писать сообщения в связи с превышением лимита предупреждений";
                 default:
                     return "Неизвестная ошибка";
             }
         }
-        public void Mute()
+        public void Mute(int minuts)
         {
             try
             {
                 IsMuted = true;
-                UnmuteTime=DateTime.Now.AddHours(30);
+                UnmuteTime=DateTime.Now.AddMinutes(minuts);
                 BotDatabase.db.SaveChanges();
                 //Send a request to telegram api and mute user
                 using (HttpClientHandler hndl = new HttpClientHandler())
                 {
                     using (HttpClient cln = new HttpClient())
                     {
-                        string restext = $"https://api.telegram.org/bot{Program.botToken}/restrictChatMember?user_id={TelegramUserId}&chat_id={Chat.TelegramChatId}";
+                        string restext = $"https://api.telegram.org/bot{Program.botToken}/restrictChatMember?user_id={TelegramUserId}&chat_id={Chat.TelegramChatId}&until_date={((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds() + 60*minuts}";
                         using (var request = cln.GetAsync(restext).Result)
                         {
 
