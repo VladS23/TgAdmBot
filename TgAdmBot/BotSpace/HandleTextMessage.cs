9dd6eebe75316ddc2b0baa6f4634a704fe3e4acf
AlexC-ux <eunv2004@gmail.com>
2022-12-25 22:11:14 +0300

    /start and /help upd

--- TgAdmBot/BotSpace/HandleTextMessage.cs
+++ TgAdmBot/BotSpace/HandleTextMessage.cs
@@ -25,6 +25,9 @@ namespace TgAdmBot.BotSpace
 
             switch (message.Text.Replace($"@{botClient.GetMeAsync().Result.Username!}", "").ToLower().Split()[0])
             {
+                case "/start":
+                    botClient.SendTextMessageAsync(message.Chat, BotPhrases.StartCommandAnswer);
+                    break;
                 case "/ranks":
                     botClient.SendTextMessageAsync(message.Chat, chat.GetRanks());
                     break;
