71a22f78a8273f35e9571b1d08301b14f694aed5
VladS23 <77384665+VladS23@users.noreply.github.com>
2022-12-26 18:25:29 +0300

    marriage class

---
+++ TgAdmBot/Database/Marriage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TgAdmBot.Database
+{
+    internal class Marriage
+    {
+        public int MarriageId { get; set; }
+        public long TelegramChatId { get; set; }
+        public DateTime DateOfConclusion { get; set; } = DateTime.Now;
+        public User User1 { get; set; }
+        public User User2 { get; set; }
+
+        public Marriage(long telegramChatId, DateTime dateOfConclusion, User user1, User user2)
+        {
+            TelegramChatId = telegramChatId;
+            DateOfConclusion = dateOfConclusion;
+            User1 = user1;
+            User2 = user2;
+            BotDatabase.db.Marriages.Single(m => m.TelegramChatId == telegramChatId).Marriages.Add(new Database.Marriage
+            {
+                TelegramChatId = telegramChatId;
+                DateOfConclusion = dateOfConclusion;
+                User1 = user1;
+                User2 = user2;
+        });
+            BotDatabase.db.SaveChanges();
+        }
+    }
+}
