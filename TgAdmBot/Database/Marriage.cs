0a4af8713e62d9f0fd93b44127a81fb409bbfeca
VladS23 <77384665+VladS23@users.noreply.github.com>
2022-12-26 18:35:55 +0300

    minor fix

--- TgAdmBot/Database/Marriage.cs
+++ TgAdmBot/Database/Marriage.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using TgAdmBot.Database;
 
 namespace TgAdmBot.Database
 {
